bab5ae5523da4ba0069cde827c3eba34de2fe37b
ARUL JAYARAJ <108589522+Aruljayaraj-AFX@users.noreply.github.com>
2025-09-17 14:40:07 +0000

    middleware

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 from routers.users import router
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app=FastAPI()
 
 app.include_router(router,prefix="/0TO100/v1/users",tags=["users"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__=="__main__":
     uvicorn.run(app,host="127.0.0.1",port=8001)
